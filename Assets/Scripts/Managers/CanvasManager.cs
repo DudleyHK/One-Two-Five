611eeca5de6066d707cb4be9b36f0f09ed5df97c
DudleyHK <dudleydawes@hotmail.co.uk>
2018-10-01 16:23:51 +0100

    Update CanvasManager

--- Assets/Scripts/Managers/CanvasManager.cs
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -9,13 +9,35 @@ using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour
 {
 
-    [SerializeField] Canvas mainCanvas;
-    [SerializeField] Text screenResolution;
-    
+    [SerializeField] private Canvas mainCanvas;
+    [SerializeField] private Text screenResolution;
 
+    [Header("Player")]
+    [SerializeField] private Slider playerSpeed;
+    [SerializeField] private Slider playerTurn;
+    [SerializeField] private Slider playerSlowDamp;
+
+    [Header("Collectables")]
+    [SerializeField] private Button collectableToggle;
+    
+    [Header("Enemies")]
+    [SerializeField] private Button kamakaziEnemyToggle;
+    [SerializeField] private Slider kamakaziEnemySpeed;
+    [SerializeField] private Slider kamakaziEnemyInterval;
+    
+    
+    private bool collectablesActive = false;
+    
 
     private void Start()
     {
         screenResolution.text = "Screen Resolution: " + Screen.currentResolution;
+        
+        collectableToggle.onClick.AddListener(ToggleCollectables);
+    }
+    
+    private void ToggleCollectables()
+    {
+        collectablesActive = !collectablesActive;
     }
 }
